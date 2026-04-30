7948f8254a05352475e0884bb9e4eb415b969f48
garrettspot <shoheadam@gmail.com>
2026-05-01 01:17:24 +0530

    add analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { ChatBox } from './components/ChatBox'
+import { Analytics } from "@vercel/analytics/next"
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="app">
       <ChatBox />
+      <Analytics></Analytics>
     </div>
   )
 }
